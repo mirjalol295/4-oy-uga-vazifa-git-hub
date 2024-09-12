ea380b737a5b326ac413aa9a5c39b81958cdf38b
Mirjalol <mirjalolahmadov0731@gmail.com>
2024-09-13 01:31:07 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Dark mode toggle
+const darkModeToggle = document.querySelector('.darmode');
+darkModeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+
+  // Toggle dark mode icon
+  const isDarkMode = document.body.classList.contains('dark-mode');
+  const darkModeIcon = document.querySelector('.darmode i');
+  if (isDarkMode) {
+    darkModeIcon.classList.replace('bx-brightness-half', 'bx-sun');
+  } else {
+    darkModeIcon.classList.replace('bx-sun', 'bx-brightness-half');
+  }
+});
+
+// Hamburger menu toggle
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const hamburgerIcon = document.querySelector('.hamburger-menu i');
+
+hamburgerMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle('show');
+  
+  // Toggle hamburger icon between 'bx-menu' and 'bx-x'
+  if (hamburgerIcon.classList.contains('bx-menu')) {
+    hamburgerIcon.classList.replace('bx-menu', 'bx-x');
+  } else {
+    hamburgerIcon.classList.replace('bx-x', 'bx-menu');
+  }
+});
